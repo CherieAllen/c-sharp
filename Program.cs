65597854bcfd3ccbaeff60fcc529fe076a19fa9b
Cherie Allen <cherie@Cheries-MacBook-Air.local>
2022-06-01 12:00:25 -0400

    added more account info

--- Program.cs
+++ Program.cs
@@ -6,4 +6,16 @@ using Accounts;
 
 BankAccount account = new BankAccount();
 
-Console.WriteLine($"This us my account number {account.AccountNumber} my bal {account.Balance}. Plus my name is {account.Owner}");
+Console.WriteLine($"This is my account number {account.AccountNumber} my bal {account.Balance}. Plus my name is {account.Owner}");
+
+BankAccount account2 = new BankAccount("Nerissa", decimal.MinValue);
+
+Console.WriteLine($"This is my account number 2 {account2.AccountNumber} my bal {account2.Balance}. Plus my name is {account2.Owner}");
+
+SavingAccount savings = new SavingAccount();
+savings.InterestRate= 0.05m;
+savings.Balance = 2000.0m;
+
+savings.AddMonthlyInterest();
+
+Console.WriteLine($"What is the balance {savings.Balance}");
