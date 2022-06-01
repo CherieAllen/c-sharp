7e3bf8314efd8a788957ede497848bf8b9c4c1ad
Cherie Allen <cherie@Cheries-MacBook-Air.local>
2022-06-01 10:55:36 -0400

    added strings and numbersmore

--- Program.cs
+++ Program.cs
@@ -22,9 +22,10 @@ Console.WriteLine($"What is 70/3 {div}");
 Console.WriteLine($"What is the maxish {maxIsh}");
 
 
-Console.WriteLine("-------------List---------");
+Console.WriteLine("-------------Array---------");
 
-var firstArr= new int[10];
+// var firstArr= new int[10];
+var firstArr= new int[5] {-69,46,10,1,100};
 int [] arr2 = new int[10];
 
 // firstarr[0]=100;
@@ -33,3 +34,20 @@ for (var i = 0; i< firstArr.Length; i++)
 {
     Console.WriteLine($"The value at {i} is {firstArr[i]}");
 }
+
+
+Console.WriteLine("-------------List---------");
+
+List <int> list = new List<int>();
+
+list.AddRange(firstArr);
+list.Add(1000);
+list.Add(1001);
+list.Add(1002);
+
+foreach(var el in list)
+{
+    Console.WriteLine($"The value is {el}");
+}
+
+Console.WriteLine ($"The first element is {list[0]}");
